c270482075343642de812edc760145f90a2f18d8
pratima <pratimatiwari87728772@gmail.com>
2026-04-06 08:42:39 +0530

    fixed allowed hosts

--- formpro/settings.py
+++ formpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8@d*i37ma%%m&c+nlxo&xb07gm2(jmq=4&vv3h38pw@)pd75za
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
